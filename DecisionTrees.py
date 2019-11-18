cc4f1b7017b027e9f4d4b4d17256ecda183050d3
Deresius <DavidMichaelSpurlock@live.com>
2019-11-17 23:49:01 -0500

    Added kfolding to decisions trees and regression

--- DecisionTrees.py
+++ DecisionTrees.py
@@ -1,5 +1,6 @@
 import sklearn
 from sklearn import tree, metrics
+from sklearn.model_selection import KFold, StratifiedKFold
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 import pandas
 import numpy as np
@@ -9,14 +10,39 @@ import os
 
 os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
 
-
-def run():
-    time1 = time.time()
+def run_kfold():    
     dataset = pandas.read_csv("processed.csv")
     dataset = dataset.dropna()
 
     le = LabelEncoder()
     encoded = dataset.apply(le.fit_transform)
+
+    ds_size = dataset.keys().size - 1
+    x = encoded.iloc[:, 0:ds_size].values
+    y = encoded.iloc[:, ds_size].values
+
+    average = 0
+    kfold_count = 10
+    ss = StratifiedKFold(n_splits=kfold_count, shuffle=False, random_state=0)
+    for train_index, test_index in ss.split(x, y):
+        x_train, x_test = x[train_index], x[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        sc = StandardScaler()
+        x_train = sc.fit_transform(x_train)
+        x_test = sc.transform(x_test)
+        treeRegressor = tree.DecisionTreeRegressor()
+        treeRegressor = treeRegressor.fit(x_train, y_train)
+        y_prediction = treeRegressor.predict(x_test)
+        average += metrics.mean_absolute_error(y_test, y_prediction)  
+    print('Average mean absolute error: ' + str((average / kfold_count)))
+
+def run():    
+    time1 = time.time()
+    dataset = pandas.read_csv("processed.csv")
+    dataset = dataset.dropna()
+
+    le = LabelEncoder()
+    encoded = dataset.apply(le.fit_transform)    
 
     ds_size = dataset.keys().size - 1
     x = encoded.iloc[:, 0:ds_size].values
@@ -30,15 +56,12 @@ def run():
 
     treeRegressor = tree.DecisionTreeRegressor()
     treeRegressor = treeRegressor.fit(x_train, y_train)
-
     # print(tree.plot_tree(treeRegressor))
-    tree.plot_tree(treeRegressor)
-    dot_data = tree.export_graphviz(treeRegressor, out_file=None)
-    graph = graphviz.Source(dot_data)
-    graph.render("connecticut")
-
+    # tree.plot_tree(treeRegressor)
+    # dot_data = tree.export_graphviz(treeRegressor, out_file=None)
+    # graph = graphviz.Source(dot_data)
+    # graph.render("connecticut")
     y_prediction = treeRegressor.predict(x_test)
-
     time2 = time.time()
     print('It took %s seconds to load and train the data.' % '{0:,.2f}'.format((time2 - time1)))
     print('Mean Absolute Error:', '{0:,.2f}'.format(metrics.mean_absolute_error(y_test, y_prediction)))
--- Regression.py
+++ Regression.py
@@ -4,11 +4,35 @@ from sklearn import preprocessing
 from sklearn.preprocessing import LabelEncoder
 import pandas as pd
 import time
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, StratifiedKFold
 from sklearn.preprocessing import StandardScaler
 from sklearn import metrics
 import matplotlib.pyplot as plt
 
+
+def run_kfold():
+    dataset = pd.read_csv(f'processed.csv')
+    dataset.dropna(inplace=True)
+    le = LabelEncoder()
+    encoded_dataset = dataset.apply(le.fit_transform)
+    ds_size = dataset.keys().size - 1
+    x = encoded_dataset.iloc[:, 0:ds_size].values
+    y = encoded_dataset.iloc[:, ds_size].values
+    average = 0
+    kfold_count = 10
+    ss = StratifiedKFold(n_splits=kfold_count, shuffle=False, random_state=0)
+    for train_index, test_index in ss.split(x, y):
+        print('new fold')
+        x_train, x_test = x[train_index], x[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        sc = StandardScaler()
+        x_train = sc.fit_transform(x_train)
+        x_test = sc.transform(x_test)
+        regressor = linear_model.LinearRegression()
+        regressor.fit(x_train, y_train)
+        y_prediction = regressor.predict(x_test)
+        average += metrics.mean_absolute_error(y_test, y_prediction)  
+    print('Average mean absolute error: ' + str((average / kfold_count)))
 
 def run():
     time1 = time.time()
