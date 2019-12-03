8b6e78642447790450b4b50a727e208b83ddf158
Jacob Slattery <jslatte1@my.westga.edu>
2019-12-02 21:34:20 -0500

    Added ability to find best of kfold to forest

--- Forests.py
+++ Forests.py
@@ -1,12 +1,17 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.model_selection import StratifiedKFold
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
 from sklearn import metrics
+import copy
+import time as t
 
 
 def run():
+    time1 = t.time()
     print('RF Reading...:')
     # Read
     dataset = pd.read_csv(f'processed.csv')
@@ -25,11 +30,13 @@ def run():
     print('RF Setup data...:')
     # Setup data
     ds_size = dataset.keys().size - 1
-    x = encoded_dataset.iloc[:, 0:ds_size].values
-    y = encoded_dataset.iloc[:, ds_size].values
+    x = encoded_dataset.iloc[:10000, 0:ds_size].values
+    y = encoded_dataset.iloc[:10000, ds_size].values
 
     err = 0
-    kfold_count = 5
+    kfold_count = 10
+    best = 100000000000
+    best_model = None
     ss = StratifiedKFold(n_splits=kfold_count, shuffle=True, random_state=42)
     for train_index, test_index in ss.split(x, y):
         x_train, x_test = x[train_index], x[test_index]
@@ -43,11 +50,28 @@ def run():
 
         print('RF Training...:')
         # Train
-        regressor = RandomForestRegressor(n_estimators=100, max_depth=5, max_leaf_nodes=15, random_state=0)
+        regressor = RandomForestRegressor(n_estimators=10, max_depth=50, max_leaf_nodes=300, random_state=0)
         regressor.fit(x_train, y_train)
         y_prediction = regressor.predict(x_test)
 
+        curr_err = np.sqrt(metrics.mean_absolute_error(y_test, y_prediction))
+        err += curr_err
+        print(curr_err)
 
-        err += np.sqrt(metrics.mean_absolute_error(y_test, y_prediction))
-        print(np.sqrt(metrics.mean_absolute_error(y_test, y_prediction)))
-    print('Average Root Mean Squared Error: ', err/kfold_count)
+        if best is None or best > curr_err:
+            best_model = copy.deepcopy(regressor)
+
+    print('Average Absolute Mean Error: ', err / kfold_count)
+
+    x = sc.transform(x)
+    y_prediction = best_model.predict(x)
+
+    time2 = t.time()
+    print("Forest took %s minutes to complete" % ((time2 - time1) / 60))
+    print("Importances: ", best_model.feature_importances_)
+    print('Mean Absolute Error On Whole Dataset:', metrics.median_absolute_error(y, y_prediction))
+    plt.figure()
+    plt.plot(y_prediction[0:30], label='pred')
+    plt.plot(y_test[0:30], 'gd', label='test')
+    plt.legend(loc="best")
+    plt.show()
